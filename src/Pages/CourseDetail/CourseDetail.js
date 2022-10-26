77154467d368fbb12ca8195a49edc2e9ccec853e
maruf <dev.maruf112@gmail.com>
2022-10-26 20:56:27 +0600

    pdf updated

--- src/Pages/CourseDetail/CourseDetail.js
+++ src/Pages/CourseDetail/CourseDetail.js
@@ -24,21 +24,22 @@ const CourseDetail = () => {
           )}
         </Pdf>
       </div>
-
-      <Card imgSrc={img}>
-        <h3 className="font-bold text-2xl">{name}</h3>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {desc.length > 250 ? desc.slice(0, 250) + "..." : desc}
-        </p>
-        <ul ref={ref} style={{ listStyle: "square", marginLeft: "16px" }}>
-          {features.map((feature) => (
-            <li>{feature}</li>
-          ))}
-        </ul>
-        <Button gradientDuoTone="cyanToBlue">
-          <Link to={`/checkout/${id}`}>Get Premium Access </Link>
-        </Button>
-      </Card>
+      <div ref={ref}>
+        <Card imgSrc={img}>
+          <h3 className="font-bold text-2xl">{name}</h3>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {desc.length > 250 ? desc.slice(0, 250) + "..." : desc}
+          </p>
+          <ul style={{ listStyle: "square", marginLeft: "16px" }}>
+            {features.map((feature) => (
+              <li>{feature}</li>
+            ))}
+          </ul>
+          <Button gradientDuoTone="cyanToBlue">
+            <Link to={`/checkout/${id}`}>Get Premium Access </Link>
+          </Button>
+        </Card>
+      </div>
     </div>
   );
 };
