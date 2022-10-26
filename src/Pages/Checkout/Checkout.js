f936232eea02617e23d1357eb9492611850e0d6f
maruf <dev.maruf112@gmail.com>
2022-10-27 05:59:25 +0600

    checkout updated

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,15 @@
 import { Button, Card } from "flowbite-react";
 import React from "react";
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../context/UserContext";
 
 const Checkout = () => {
   const checkoutCourses = useLoaderData();
-  console.log(checkoutCourses);
+  const { user } = useContext(AuthContext);
+  // console.log(user);
+  const { displayName, email } = user;
+  // console.log(checkoutCourses);
   const { name, price, features } = checkoutCourses;
   return (
     <div>
@@ -13,6 +18,7 @@ const Checkout = () => {
       </h2>
       <div className="max-w-sm mx-auto">
         <Card>
+          {}
           <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
             Standard plan
           </h5>
@@ -53,6 +59,35 @@ const Checkout = () => {
             className="inline-flex w-full justify-center rounded-lg bg-green-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
           >
             Choose plan
+          </Button>
+        </Card>
+        <Card className="mt-5">
+          <h5 className="text-2xl font-semi tracking-tight text-gray-900 dark:text-white">
+            Item : {name}
+          </h5>
+          <p className="font-medium text-gray-700 dark:text-gray-400">
+            Ordered by : {displayName}
+          </p>
+          <p className="font-medium text-gray-700 dark:text-gray-400">
+            Email : {email}
+          </p>
+          <p className="font-medium text-gray-700 dark:text-gray-400">
+            Total Price : ${price}
+          </p>
+          <Button gradientDuoTone="greenToBlue">
+            Checkout
+            <svg
+              className="ml-2 -mr-1 h-4 w-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
           </Button>
         </Card>
       </div>
