4bc7a76e20e77682f7ebc468d8c537a7dc90f033
maruf <dev.maruf112@gmail.com>
2022-10-26 08:38:19 +0600

    course data set from server

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-
+import CoursesData from "../CoursesData/CoursesData";
+import "./courses.css";
 const Courses = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const courses = useLoaderData();
+  console.log(courses);
 
   return (
     <div>
-      <h2>Courses page.</h2>
+      <div className="courses-container">
+        <div className="courses-sidebar">
+          {courses.map((course) => (
+            <p>{course.name}</p>
+          ))}
+        </div>
+        <div className="courses-content">
+          {courses.map((course) => (
+            <CoursesData key={course.id} course={course}></CoursesData>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
