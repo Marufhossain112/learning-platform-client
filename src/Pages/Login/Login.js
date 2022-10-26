b1049e5ca411c4fa415da1048ced0307047e690d
maruf <dev.maruf112@gmail.com>
2022-10-27 05:02:08 +0600

    responsiveness done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -21,6 +21,7 @@ const Login = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        navigate(from, { replace: true });
       })
       .catch((error) => {
         console.error(error);
@@ -32,6 +33,7 @@ const Login = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        navigate(from, { replace: true });
       })
       .catch((error) => {
         console.error(error);
